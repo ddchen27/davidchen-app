1301c09d2f1bde41784bf0bd606dbab213d1b5d0
ddchen27 <davidchen1337@gmail.com>
2023-07-20 16:25:57 -0700

    started website design

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+// import Blog from "../templates/blog/Blog.js";
+// import Dashboard from "../templates/dashboard/Dashboard.js";
+
+// import Album from "../templates/album/Album.js";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
